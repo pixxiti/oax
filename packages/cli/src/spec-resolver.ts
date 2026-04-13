df4f28b10c4161cf22fb91ef2dc7dcb905ec8919
Brent Kirkland <brentkirkland@users.noreply.github.com>
2026-04-13 12:25:40 -0700

    chore(cli): remove unused os import from spec-resolver

--- packages/cli/src/spec-resolver.ts
+++ packages/cli/src/spec-resolver.ts
@@ -1,6 +1,5 @@
 import { execFileSync } from "node:child_process";
 import * as fs from "node:fs";
-import * as os from "node:os";
 import * as path from "node:path";
 import yaml from "js-yaml";
 import type { Source } from "./manifest";
